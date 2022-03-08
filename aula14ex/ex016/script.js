eb77c5afdff2f238fab6e773f8256a64a27e6d98
Gustavo dos Santos <95661737+DevGustavoSantos@users.noreply.github.com>
2022-03-08 13:16:27 -0300

    aula 14

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,34 @@
+//Conexão das variáveis com os inputs
+var inicio_txt = window.document.getElementById('inicio')
+var fim_txt = window.document.getElementById('fim')
+var passo_txt = window.document.getElementById('passo')
+var botao = window.document.getElementById('botao')
+var resultado = window.document.getElementById('resultado')
+
+botao.addEventListener('click', contar)
+
+function contar() {
+    /* Transformando inputs strings em numbers */
+    var inicio_num = Number.parseInt(inicio_txt.value)
+    var fim_num = Number.parseInt(fim_txt.value)
+    var passo_num = Number.parseInt(passo_txt.value)
+
+
+
+  /*   window.alert(`O inicio ${inicio_num}, o fim ${fim_num}, e o passo ${passo_num}.`) */
+    if (isNaN(inicio_num) || isNaN(fim_num) || isNaN(passo_num)) {
+        window.alert('Faltam valores!!! Por favor verifique os campos e tente novamente.')
+    } else if (passo_num <= 0){
+        window.alert('O passo não pode ser menor ou igual a zero!!! Por favor verifique os campos e tente novamente.')
+    } else {
+        resultado.innerHTML = `Contando...<br>`
+        for (var cont = inicio_num; cont < fim_num || cont == fim_num; cont += passo_num) {
+            /* window.alert(`${cont}`) */
+            var texto = document.createElement('span')
+            texto.innerHTML = `${cont} &#x1F449;`
+            resultado.appendChild(texto)
+        }
+        texto.innerHTML = `&#x1F3C1;`
+    }
+
+}
